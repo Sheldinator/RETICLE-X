0b802321e7dbfd5e7c7a1f99426e74ba07b57c3d
Sheldinator <56585962+Sheldinator@users.noreply.github.com>
2020-03-16 22:01:39 +0100

    First Commit Reticle-X

---
+++ Assets/Shoot.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+public class Shoot : MonoBehaviour{
+
+
+
+   public float damage = 10f;
+   public float range = 100f;
+   public float fireRate = 15f;
+
+   public Camera fpsCam;
+
+   private float nextTimeToFire - 0f;
+
+    
+    void Update() {
+
+    if (Input.GetButtonDown("Fire1"))Time.time >= nextTimeToFire;
+        {
+            nextTimeToFire = Time.time + 1f/fireRate;
+            Shot();
+        }
+}
+void Shot ()
+{
+     RaycastHit hit;
+     if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+     {
+       Debug.Log(hit.transform.name);
+
+       Target target = hit.transform.GetComponent<Target>();
+       if (target != null)
+        {
+            target.TakeDamage(damage);
+        }
+     }
+   }
+}
+
+
+  
